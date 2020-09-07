26613b7add0e3485907e9cca833ccfc307aecd84
Ragul28 <ragulh28@gmail.com>
2020-09-07 22:12:27 +0530

    Updated cli with status sub command

---
+++ cmd/status.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/ragul28/svault/vault"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get the status of the vault engine",
+	Run: func(cmd *cobra.Command, args []string) {
+		vault.StatusVault()
+	},
+}
