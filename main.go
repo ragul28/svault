36d04b0c333e9d6ef31b2f0506295eb56447df81
ragul28 <ragulh28@gmail.com>
2020-09-07 22:39:12 +0530

    Added go mod vendor

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"github.com/ragul28/svault/vault"
+	"github.com/ragul28/svault/cmd"
 )
 
 func main() {
 
-	MasterKey := []byte(vault.VaultInit(true))
-
-	vault.WriteVault(MasterKey, "secret_key", "supersecrettoken")
-	vault.ReadVault(MasterKey, "secret_key")
-
-	vault.StatusVault()
-	vault.ListVault()
+	cmd.Execute()
 
 }
